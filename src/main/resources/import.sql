51c7a02f3354ce306bc40e531b49d2ee10853085
Adroaldo <hygino@msn.com>
2022-05-20 22:52:04 -0300

    Fixed error on import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,29 +1,19 @@
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Gorete Medeiros', '89631139-11', 1345.63, TIMESTAMP WITH TIME ZONE '1987-03-18T20:50:07.12345:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Gorete Medeiros', '89631139-11', 1345.63, TIMESTAMP WITH TIME ZONE '1987-03-18T20:50:07.12345Z', 1);
 
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Jupira da Silva', '72842870-32', 2347.26, TIMESTAMP WITH TIME ZONE '1978-09-28T14:23:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jupira da Silva', '72842870-32', 2347.26, TIMESTAMP WITH TIME ZONE '1978-09-28T14:23:07.12345Z', 3);
 
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Godofredo Almeida', '44519536-21', 1867.14, TIMESTAMP WITH TIME ZONE '1997-10-17T22:10:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Godofredo Almeida', '44519536-21', 1867.14, TIMESTAMP WITH TIME ZONE '1997-10-17T22:10:07.12345Z', 2);
 
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Antonio Trindade', '33396948-16', 2358.94, TIMESTAMP WITH TIME ZONE '1986-04-08T22:33:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Antonio Trindade', '33396948-16', 2358.94, TIMESTAMP WITH TIME ZONE '1986-04-08T22:33:07.12345Z', 0);
 
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Juvenal Cardoso', '69669405-27', 2418.43, TIMESTAMP WITH TIME ZONE '1981-06-18T12:45:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Juvenal Cardoso', '69669405-27', 2418.43, TIMESTAMP WITH TIME ZONE '1981-06-18T12:45:07.12345Z', 2);
 
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Reginaldo Vanini', '31339260-16', 3147.22, TIMESTAMP WITH TIME ZONE '1989-06-07T10:23:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Reginaldo Vanini', '31339260-16', 3147.22, TIMESTAMP WITH TIME ZONE '1989-06-07T10:23:07.12345Z', 0);
 
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Elevino Nunes', '13069066-41', 2492.95, TIMESTAMP WITH TIME ZONE '1988-05-20T16:57:07.12345Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Elevino Nunes', '13069066-41', 2492.95, TIMESTAMP WITH TIME ZONE '1988-05-20T16:57:07.12345Z', 4);
 
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Maira da Costa', '66889139-19', 4325.15, TIMESTAMP WITH TIME ZONE '1983-10-30T23:10:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maira da Costa', '66889139-19', 4325.15, TIMESTAMP WITH TIME ZONE '1983-10-30T23:10:07.12345Z', 3);
 
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Fatima Zelin', '53829412-35', 2145.32, TIMESTAMP WITH TIME ZONE '1982-07-29T11:17:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fatima Zelin', '53829412-35', 2145.32, TIMESTAMP WITH TIME ZONE '1982-07-29T11:17:07.12345Z', 0);
 
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES 
-('Jurema da Silva', '76812113-31', 2244.23, TIMESTAMP WITH TIME ZONE '1974-03-18T18:16:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jurema da Silva', '76812113-31', 2244.23, TIMESTAMP WITH TIME ZONE '1974-03-18T18:16:07.12345Z', 1);
